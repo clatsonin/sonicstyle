2e6b01fb5b672fee428c4ad3daf666087565f31a
Clatson <124655279+clatsonin@users.noreply.github.com>
2024-07-04 11:56:09 +0530

    Update main.py

--- fashion_recommentaion_fastapi/main.py
+++ fashion_recommentaion_fastapi/main.py
@@ -1,1 +1,8 @@
 #Setup Environment Variables
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("GOOGLE_API_KEY")
+if not api_key:
+    raise ValueError("GOOGLE_API_KEY is not set in the environment variables")
