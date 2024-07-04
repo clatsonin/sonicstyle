60000c6e7262b34a6aff023277f1026b984c2bd0
Clatson <124655279+clatsonin@users.noreply.github.com>
2024-07-04 11:57:48 +0530

    Update main.py

--- fashion_recommentaion_fastapi/main.py
+++ fashion_recommentaion_fastapi/main.py
@@ -6,3 +6,6 @@ load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
 if not api_key:
     raise ValueError("GOOGLE_API_KEY is not set in the environment variables")
+
+#Install Required Packages
+#pip install fastapi pydantic uvicorn python-dotenv google-generativeai
